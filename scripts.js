66c94ed4e185a12f9747b76f60a034c55ead42dc
Sibulele Ngada <sibulelengada@gmail.com>
2024-11-01 13:47:44 +0200

    Wrap function logic in try catches

--- scripts.js
+++ scripts.js
@@ -8,67 +8,81 @@ const html = {
 };
 
 function displayBooks(books) {
-  const fragment = document.createDocumentFragment();
+  try {
+    const fragment = document.createDocumentFragment();
 
-  for (const { author, id, image, title } of books.slice(
-    booksDisplayed,
-    booksDisplayed + BOOKS_PER_PAGE
-  )) {
-    const element = document.createElement("button");
-    element.classList = "preview";
-    element.setAttribute("data-preview", id);
+    for (const { author, id, image, title } of books.slice(
+      booksDisplayed,
+      booksDisplayed + BOOKS_PER_PAGE
+    )) {
+      const element = document.createElement("button");
+      element.classList = "preview";
+      element.setAttribute("data-preview", id);
 
-    element.innerHTML = `
-              <img
-                  class="preview__image"
-                  src="${image}"
-              />
-              
-              <div class="preview__info">
-                  <h3 class="preview__title">${title}</h3>
-                  <div class="preview__author">${authors[author]}</div>
-              </div>
-          `;
+      element.innerHTML = `
+                    <img
+                        class="preview__image"
+                        src="${image}"
+                    />
+                    
+                    <div class="preview__info">
+                        <h3 class="preview__title">${title}</h3>
+                        <div class="preview__author">${authors[author]}</div>
+                    </div>
+                `;
 
-    fragment.appendChild(element);
+      fragment.appendChild(element);
+    }
+
+    document.querySelector("[data-list-items]").appendChild(fragment);
+    booksDisplayed += BOOKS_PER_PAGE;
+  } catch (err) {
+    console.error(`There's been an issue with displaying books - ${err}`);
   }
-
-  document.querySelector("[data-list-items]").appendChild(fragment);
-  booksDisplayed += BOOKS_PER_PAGE;
 }
 
 function updateUI(books) {
-  displayBooks(books);
-  document.querySelector("[data-list-button]").disabled = false;
-  document.querySelector("[data-list-button]").innerText =
-    `Show more (${books.length - booksDisplayed})`;
+  try {
+    displayBooks(books);
+    document.querySelector("[data-list-button]").disabled = false;
+    document.querySelector("[data-list-button]").innerText =
+      `Show more (${books.length - booksDisplayed})`;
 
-  if (books.length - booksDisplayed <= 0) {
-    document.querySelector("[data-list-button]").innerText = `Show more (0)`;
-    document.querySelector("[data-list-button]").disabled = true;
+    if (books.length - booksDisplayed <= 0) {
+      document.querySelector("[data-list-button]").innerText = `Show more (0)`;
+      document.querySelector("[data-list-button]").disabled = true;
+    }
+  } catch (err) {
+    console.error(`There's been an issue with updating the UI - ${err}`);
   }
 }
 
 function generateFilter(filter) {
-  const filterName = filter === authors ? "Authors" : "Genres";
+  try {
+    const filterName = filter === authors ? "Authors" : "Genres";
 
-  const fragment = document.createDocumentFragment();
-  const firstElement = document.createElement("option");
-  firstElement.value = "any";
-  firstElement.innerText = `All ${filterName}`;
-  fragment.appendChild(firstElement);
-  console.log;
+    const fragment = document.createDocumentFragment();
+    const firstElement = document.createElement("option");
+    firstElement.value = "any";
+    firstElement.innerText = `All ${filterName}`;
+    fragment.appendChild(firstElement);
+    console.log;
 
-  for (const [id, name] of Object.entries(filter)) {
-    const element = document.createElement("option");
-    element.value = id;
-    element.innerText = name;
-    fragment.appendChild(element);
+    for (const [id, name] of Object.entries(filter)) {
+      const element = document.createElement("option");
+      element.value = id;
+      element.innerText = name;
+      fragment.appendChild(element);
+    }
+
+    document
+      .querySelector(`[data-search-${filterName.toLowerCase()}]`)
+      .appendChild(fragment);
+  } catch (err) {
+    console.error(
+      `There's been an issue with generating a search filter - ${err}`
+    );
   }
-
-  document
-    .querySelector(`[data-search-${filterName.toLowerCase()}]`)
-    .appendChild(fragment);
 }
 
 function loadSearch() {
@@ -77,159 +91,188 @@ function loadSearch() {
 }
 
 function toggleTheme(theme) {
-  if (
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches &&
-    theme === "night"
-  ) {
-    html.theme.value = "night";
-    document.documentElement.style.setProperty("--color-dark", "255, 255, 255");
-    document.documentElement.style.setProperty("--color-light", "10, 10, 20");
-  } else {
-    html.theme.value = "day";
-    document.documentElement.style.setProperty("--color-dark", "10, 10, 20");
-    document.documentElement.style.setProperty(
-      "--color-light",
-      "255, 255, 255"
-    );
+  try {
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches &&
+      theme === "night"
+    ) {
+      html.theme.value = "night";
+      document.documentElement.style.setProperty(
+        "--color-dark",
+        "255, 255, 255"
+      );
+      document.documentElement.style.setProperty("--color-light", "10, 10, 20");
+    } else {
+      html.theme.value = "day";
+      document.documentElement.style.setProperty("--color-dark", "10, 10, 20");
+      document.documentElement.style.setProperty(
+        "--color-light",
+        "255, 255, 255"
+      );
+    }
+  } catch (err) {
+    console.error(`There's been an issue with toggling the theme - ${err}`);
   }
 }
 
 function selectedBook(active, pathArray) {
-  for (const node of pathArray) {
-    if (active) break;
+  try {
+    for (const node of pathArray) {
+      if (active) break;
 
-    if (node?.dataset?.preview) {
-      let result = null;
+      if (node?.dataset?.preview) {
+        let result = null;
 
-      for (const singleBook of books) {
-        if (result) break;
-        if (singleBook.id === node?.dataset?.preview) result = singleBook;
+        for (const singleBook of books) {
+          if (result) break;
+          if (singleBook.id === node?.dataset?.preview) result = singleBook;
+        }
+
+        active = result;
       }
-
-      active = result;
     }
-  }
 
-  if (active) {
-    document.querySelector("[data-list-active]").open = true;
-    document.querySelector("[data-list-blur]").src = active.image;
-    document.querySelector("[data-list-image]").src = active.image;
-    document.querySelector("[data-list-title]").innerText = active.title;
-    document.querySelector("[data-list-subtitle]").innerText =
-      `${authors[active.author]} (${new Date(active.published).getFullYear()})`;
-    document.querySelector("[data-list-description]").innerText =
-      active.description;
+    if (active) {
+      document.querySelector("[data-list-active]").open = true;
+      document.querySelector("[data-list-blur]").src = active.image;
+      document.querySelector("[data-list-image]").src = active.image;
+      document.querySelector("[data-list-title]").innerText = active.title;
+      document.querySelector("[data-list-subtitle]").innerText =
+        `${authors[active.author]} (${new Date(active.published).getFullYear()})`;
+      document.querySelector("[data-list-description]").innerText =
+        active.description;
+    }
+  } catch (err) {
+    console.error(`There's been an issue with selecting a book - ${err}`);
   }
 }
 
 function showResults(result) {
-  document.querySelector("[data-list-items]").innerHTML = "";
-  booksDisplayed = 0;
-  updateUI(result);
-  window.scrollTo({ top: 0, behavior: "smooth" });
+  try {
+    document.querySelector("[data-list-items]").innerHTML = "";
+    booksDisplayed = 0;
+    matches = result;
+
+    if (result.length < 1) {
+      document
+        .querySelector("[data-list-message]")
+        .classList.add("list__message_show");
+    } else {
+      document
+        .querySelector("[data-list-message]")
+        .classList.remove("list__message_show");
+    }
+
+    updateUI(result);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  } catch (err) {
+    console.error(
+      `There's been an issue with displaying search results - ${err}`
+    );
+  }
 }
 
 function search(filters) {
-  const result = [];
+  try {
+    const result = [];
 
-  for (const book of books) {
-    let genreMatch = filters.genre === "any";
+    for (const book of books) {
+      let genreMatch = filters.genre === "any";
 
-    for (const singleGenre of book.genres) {
-      if (genreMatch) break;
-      if (singleGenre === filters.genre) {
-        genreMatch = true;
+      for (const singleGenre of book.genres) {
+        if (genreMatch) break;
+        if (singleGenre === filters.genre) {
+          genreMatch = true;
+        }
+      }
+
+      if (
+        (filters.title.trim() === "" ||
+          book.title.toLowerCase().includes(filters.title.toLowerCase())) &&
+        (filters.author === "any" || book.author === filters.author) &&
+        genreMatch
+      ) {
+        result.push(book);
       }
     }
 
-    if (
-      (filters.title.trim() === "" ||
-        book.title.toLowerCase().includes(filters.title.toLowerCase())) &&
-      (filters.author === "any" || book.author === filters.author) &&
-      genreMatch
-    ) {
-      result.push(book);
-    }
+    showResults(result);
+  } catch (err) {
+    console.error(`There's been an issue with searching for results - ${err}`);
   }
-
-  matches = result;
-
-  if (result.length < 1) {
-    document
-      .querySelector("[data-list-message]")
-      .classList.add("list__message_show");
-  } else {
-    document
-      .querySelector("[data-list-message]")
-      .classList.remove("list__message_show");
-  }
-
-  showResults(result);
 }
 
-document.querySelector("[data-search-cancel]").addEventListener("click", () => {
-  document.querySelector("[data-search-overlay]").open = false;
-});
+try {
+  document
+    .querySelector("[data-search-cancel]")
+    .addEventListener("click", () => {
+      document.querySelector("[data-search-overlay]").open = false;
+    });
 
-document
-  .querySelector("[data-settings-cancel]")
-  .addEventListener("click", () => {
-    document.querySelector("[data-settings-overlay]").open = false;
+  document
+    .querySelector("[data-settings-cancel]")
+    .addEventListener("click", () => {
+      document.querySelector("[data-settings-overlay]").open = false;
+    });
+
+  document
+    .querySelector("[data-header-search]")
+    .addEventListener("click", () => {
+      document.querySelector("[data-search-overlay]").open = true;
+      document.querySelector("[data-search-title]").focus();
+    });
+
+  document
+    .querySelector("[data-header-settings]")
+    .addEventListener("click", () => {
+      document.querySelector("[data-settings-overlay]").open = true;
+    });
+
+  document.querySelector("[data-list-close]").addEventListener("click", () => {
+    document.querySelector("[data-list-active]").open = false;
   });
 
-document.querySelector("[data-header-search]").addEventListener("click", () => {
-  document.querySelector("[data-search-overlay]").open = true;
-  document.querySelector("[data-search-title]").focus();
-});
+  document
+    .querySelector("[data-settings-form]")
+    .addEventListener("submit", (event) => {
+      event.preventDefault();
+      const formData = new FormData(event.target);
+      const { theme } = Object.fromEntries(formData);
+      document.querySelector("[data-settings-overlay]").open = false;
+      toggleTheme(theme);
+    });
 
-document
-  .querySelector("[data-header-settings]")
-  .addEventListener("click", () => {
-    document.querySelector("[data-settings-overlay]").open = true;
-  });
+  document
+    .querySelector("[data-search-form]")
+    .addEventListener("submit", (event) => {
+      event.preventDefault();
+      const formData = new FormData(event.target);
+      const filters = Object.fromEntries(formData);
+      document.querySelector("[data-search-overlay]").open = false;
+      search(filters);
+    });
 
-document.querySelector("[data-list-close]").addEventListener("click", () => {
-  document.querySelector("[data-list-active]").open = false;
-});
+  document
+    .querySelector("[data-list-button]")
+    .addEventListener("click", () => updateUI(matches));
 
-document
-  .querySelector("[data-settings-form]")
-  .addEventListener("submit", (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const { theme } = Object.fromEntries(formData);
-    document.querySelector("[data-settings-overlay]").open = false;
-    toggleTheme(theme);
-  });
+  document
+    .querySelector("[data-list-items]")
+    .addEventListener("click", (event) => {
+      const pathArray = Array.from(event.path || event.composedPath());
+      let active = null;
 
-document
-  .querySelector("[data-search-form]")
-  .addEventListener("submit", (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const filters = Object.fromEntries(formData);
-    document.querySelector("[data-search-overlay]").open = false;
-    search(filters);
-  });
-
-document
-  .querySelector("[data-list-button]")
-  .addEventListener("click", () => updateUI(matches));
-
-document
-  .querySelector("[data-list-items]")
-  .addEventListener("click", (event) => {
-    const pathArray = Array.from(event.path || event.composedPath());
-    let active = null;
-
-    selectedBook(active, pathArray);
-  });
+      selectedBook(active, pathArray);
+    });
+} catch (err) {
+  console.error(`There's been an issue with an event listener - ${err}`);
+}
 
 function init() {
   matches = books;
   booksDisplayed = 0;
-  html.theme.value = "night";
+  toggleTheme(html.theme.value);
   loadSearch();
   updateUI(matches);
 }
